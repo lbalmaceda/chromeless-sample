37355040b46a8087008a04117370e80b868f0fda
Luciano Balmaceda <balmacedaluciano@gmail.com>
2018-02-22 18:27:16 -0300

    change test script to work for webapp quickstarts

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
 const { Chromeless } = require('chromeless')
 const path = require('path');
 
-async function run() {
-  const chromeless = new Chromeless()
+const chromeless = new Chromeless()
 
+
+async function run() {
+  //Open the QuickStart Home page
   const screenshot = await chromeless
-    .goto('https://www.google.com')
-    .type('chromeless', 'input[name="q"]')
-    .press(13)
-    .wait('#resultStats')
-    .screenshot({ filePath: path.join(__dirname, 'out/google-search.png') })
+    .goto("http://localhost:3000")
+    .wait("#qsLoginBtn")
+    // Click log in, wait for Lock and fill the form
+    .click("#qsLoginBtn")
+    .wait("input[name='password']")
+    .wait(3000)
+    .type("asdasd", "input[name='username']")
+    .type("asdasd", "input[name='password']")
+    //Submit form and wait for any errors
+    .click(".auth0-lock-widget-container .auth0-lock-submit")
+    .wait(5000)
+    .wait("#qsLogoutBtn")
+    .screenshot({ filePath: path.join(__dirname, 'out/logged-in.png') });
 
   console.log(screenshot) // prints local file path or S3 url
 
